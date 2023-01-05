dccc7817b1edcf261449d7eecd89c7459b2b558a
Brian Bulowicz <bajebi.bb@gmail.com>
2023-01-05 14:59:15 -0600

    just cant get delete to work

--- public/notes.js
+++ public/notes.js
@@ -1,110 +1,110 @@
 
-console.log('notesjs is connected')
-const noteTitle=document.querySelector('input.note-title')
-console.log(noteTitle)
-const noteInput=document.querySelector('textarea') 
-console.log(noteInput)
-const addButton=document.querySelector('i.fa-plus')
-console.log(addButton)
-const existingNotes=document.querySelector('div.card')
-console.log(existingNotes)
-// const trashButton=document.querySelector('bi-trash')
-// console.log(trashButton)
-// const ageInput=document.querySelector('[name="age"]')
+// console.log('notesjs is connected')
+// const noteTitle=document.querySelector('input.note-title')
+// console.log(noteTitle)
+// const noteInput=document.querySelector('textarea') 
+// console.log(noteInput)
+// const addButton=document.querySelector('i.fa-plus')
+// console.log(addButton)
+// const existingNotes=document.querySelector('div.card')
+// console.log(existingNotes)
+// // const trashButton=document.querySelector('bi-trash')
+// // console.log(trashButton)
+// // const ageInput=document.querySelector('[name="age"]')
 
-// const typeInput=document.querySelector('[name="type"]')
+// // const typeInput=document.querySelector('[name="type"]')
 
-const renderNotes=(notes)=>{
-    console.log(notes)  //shows an array of the notews
-    existingNotes.innerHTML=''
-    console.log(existingNotes.innerHTML)    //is empty on console.log but data persits on page
-    notes.forEach(note=>{
-        const li=document.createElement('li')
-        // add the id of each button 5 lines down with button data-id="${note.id}"
-        // <div class="card">
-        // <button data-id="${note.id}"><i class="bi bi-trash"></i></button>
-        console.log(note)
-        console.log(`${note.title}`)
-        console.log(`${note.noteText}`)
-        console.log(`${note.id}`)
-        li.innerHTML=`
-        <h3>${note.title}</h3>
-        <p>${note.noteText}</p>
-        <i class="bi bi-trash" data-id="${note.id}" value="trash"></i>
-        </div>
-        `
-        existingNotes.appendChild(li)
-    })
-}
-/////////////////////////////////////
-//add event listener to the parent to detect clicks on children to delete selected data
-existingNotes.addEventListener('click',event=>{
-    console.log(event.target)
-    //is the event.target a button?
-    console.log(event.target)
+// const renderNotes=(notes)=>{
+//     console.log(notes)  //shows an array of the notews
+//     existingNotes.innerHTML=''
+//     console.log(existingNotes.innerHTML)    //is empty on console.log but data persits on page
+//     notes.forEach(note=>{
+//         const li=document.createElement('li')
+//         // add the id of each button 5 lines down with button data-id="${note.id}"
+//         // <div class="card">
+//         // <button data-id="${note.id}"><i class="bi bi-trash"></i></button>
+//         console.log(note)
+//         console.log(`${note.title}`)
+//         console.log(`${note.noteText}`)
+//         console.log(`${note.id}`)
+//         li.innerHTML=`
+//         <h3>${note.title}</h3>
+//         <p>${note.noteText}</p>
+//         <i class="bi bi-trash" data-id="${note.id}" value="trash"></i>
+//         </div>
+//         `
+//         existingNotes.appendChild(li)
+//     })
+// }
+// /////////////////////////////////////
+// //add event listener to the parent to detect clicks on children to delete selected data
+// existingNotes.addEventListener('click',event=>{
+//     console.log(event.target)
+//     //is the event.target a button?
+//     console.log(event.target)
   
-    if(event.target.matches('trash')){
-     console.log('matches')
-        // data-id is a property on the object you can select
-        console.log(event.target.data-id)
-        const id=event.target.data-id
-        console.log(id)  //use this value to identify data to be deleted-now need a route in server
-        //NOW CODE IN FRONT END INDEX.JS so front end code sends the ID to the delete route
-        fetch(`/api/db/${id}`,{
-            method:'DELETE'     //this changes the default rout from get to delete
-        })
-        .then(response=>response.json())
-        //rerun the fetch code to re render the animals
-        .then(()=>getAllNotes())
-        .catch(error=>console.log(error))
-    }
-})
+//     if(event.target.matches('trash')){
+//      console.log('matches')
+//         // data-id is a property on the object you can select
+//         console.log(event.target.data-id)
+//         const id=event.target.data-id
+//         console.log(id)  //use this value to identify data to be deleted-now need a route in server
+//         //NOW CODE IN FRONT END INDEX.JS so front end code sends the ID to the delete route
+//         fetch(`/api/db/${id}`,{
+//             method:'DELETE'     //this changes the default rout from get to delete
+//         })
+//         .then(response=>response.json())
+//         //rerun the fetch code to re render the animals
+//         .then(()=>getAllNotes())
+//         .catch(error=>console.log(error))
+//     }
+// })
 
-///////////////////////////////////////
-function handleSubmit(event){
-    console.log(event)       //works
-    event.preventDefault()
-    console.log(noteTitle.value)//works
-    console.log(noteInput.value)
+// ///////////////////////////////////////
+// function handleSubmit(event){
+//     console.log(event)       //works
+//     event.preventDefault()
+//     console.log(noteTitle.value)//works
+//     console.log(noteInput.value)
    
 
-    const newNote={
-        // name:nameInput.value,
-        // // age:parseInt(ageInput.value),
-        // type:typeInput.value,
-        title:noteTitle.value,
-        noteText: noteInput.value,
+//     const newNote={
+//         // name:nameInput.value,
+//         // // age:parseInt(ageInput.value),
+//         // type:typeInput.value,
+//         title:noteTitle.value,
+//         noteText: noteInput.value,
        
-        // id:instance(),
-    }
-    console.log(newNote)   //works
+//         // id:instance(),
+//     }
+//     console.log(newNote)   //works
 
 
-    fetch('api/db',{
-        method:'POST',      //posts to app.post('api/db')
-        headers: {          //which then responds at the .then
-         "Content-Type":"application/json"   
-        },
-        body:JSON.stringify(newNote)
-    })
-    .then(response=>response.json())
-    // .then(animal=>console.log(animal))
-    // .then(db=>{
-    //     window.location.href='/'    //relocates user to home screen after data submit pressed
-    // })
-    .catch(error=>console.log(error))
-    }
+//     fetch('api/db',{
+//         method:'POST',      //posts to app.post('api/db')
+//         headers: {          //which then responds at the .then
+//          "Content-Type":"application/json"   
+//         },
+//         body:JSON.stringify(newNote)
+//     })
+//     .then(response=>response.json())
+//     // .then(animal=>console.log(animal))
+//     // .then(db=>{
+//     //     window.location.href='/'    //relocates user to home screen after data submit pressed
+//     // })
+//     .catch(error=>console.log(error))
+//     }
     
-    function getAllNotes(){
-        //fetch all notes on page load- need a get route in server.js
-        fetch('api/db')   //fetch is a get request
-        .then(response=>response.json())
-        .then(notes=>renderNotes(notes))
-        .catch(err=>console.log(err))
+//     function getAllNotes(){
+//         //fetch all notes on page load- need a get route in server.js
+//         fetch('api/db')   //fetch is a get request
+//         .then(response=>response.json())
+//         .then(notes=>renderNotes(notes))
+//         .catch(err=>console.log(err))
         
-        }
+//         }
         
-        getAllNotes() 
+//         getAllNotes() 
 
-    addButton.addEventListener('click',handleSubmit)
+//     addButton.addEventListener('click',handleSubmit)
     
--- server.js
+++ server.js
@@ -1,94 +1,63 @@
-const express=require('express')
-const path = require('path')
-const fs=require('fs')
-const hyperid = require('hyperid')
-const instance = hyperid()
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const hyperid = require("hyperid");
+const instance = hyperid();
 
-const shortid = require('shortid');
-
+const shortid = require("shortid");
 
 // import { nanoid } from 'nanoid'  //model.id = nanoid()   nanoid(10)
 // const { resourceLimits } = require('worker_threads')-- what is this-I didnt put it here
-const app=express()
-const PORT=3000
-const noteData=require('./db.json') //pulls in data from database
+const app = express();
+const PORT = 3000;
+const noteData = require("./db.json"); //pulls in data from database
 
-app.use(express.static('public'))  //allows public folder to be unblocked.browser can now request resources from that-public- folder
-app.use(express.json())//  this parses the body on the request into json data for you
-
+app.use(express.static("public")); //allows public folder to be unblocked.browser can now request resources from that-public- folder
+app.use(express.json()); //  this parses the body on the request into json data for you
 
 //view /html routes
 //home page path and note page path
-app.get('/',(req,res)=>{
-    // console.log(res)
-    res.sendFile(path.join(__dirname,'public','index.html'))
-    
-})
+app.get("/", (req, res) => {
+  // console.log(res)
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-app.get('/notes',(req,res)=>{
-    res.sendFile(path.join(__dirname,'public','notes.html'))
-
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 ////////////////////////////////////////////////////////////////////
 
 // load all notes on page load of notes page by sending back json data
-app.get('/api/db',(req,res)=>{
-    //TO DO READ DB.JSON CONTENTS 
-    fs.readFile(path.join(__dirname,"db.json"),'utf-8',function(err,data){
-        if (err){
-            res.status(500).json(err)
-            return
-        }
-        //if you get contents out of readFIle parse it into json
-         console.log(JSON.parse(data))
-        const json=JSON.parse(data)
-        //RESpond with THE PARSED ARRAY
-        res.json(json)
-    })
-    })
+app.get("/api/notes", (req, res) => {
+  //TO DO READ DB.JSON CONTENTS
+  fs.readFile(path.join(__dirname, "db.json"), "utf-8", function (err, data) {
+    if (err) {
+      res.status(500).json(err);
+      return;
+    }
+    //if you get contents out of readFIle parse it into json
+    console.log(JSON.parse(data));
+    const json = JSON.parse(data);
+    //RESpond with THE PARSED ARRAY
+    res.json(json);
+  });
+});
 ////////////////////////////////////////////////////////////////
 
-app.post('/api/db',(req,res)=>{
-    const {title,noteText}=req.body    //req.body is the data for a new note-is an objest
-    // console.log('hit the db POST route',req)
-    // console.log(req.body)
-    // if(!req.body.title ||req.body.noteText){
-        if(!title||!noteText){
-    res.status(400).json({error:'title or content'})
-    return
-    }
-    //newAnimal object with take on all the key value pairs from req.body by using spread operator ...
-    const newNote={  
-       ...req.body,
-    //    id: instance(),      //instance()id will be a new key value on new note object
-        id:instance(),
-    }
-    console.log(newNote)
-    //push this data into an array-THE NEXT TWO LINES WONT MAKE DATA PERSIST:
-// noteData.push(newNote)
-// res.json(newNotel)
+app.post("/api/notes", (req, res) => {
+  const noteMade = createNote(req.body, noteData);
+  res.json(noteMade);
+});
 
-//MAKE DATA PERSIST:  
-//READ CONTENTS OF DB.JSON
-fs.readFile(path.join(__dirname, "db.json"),"utf-8", function(err,data){
-if (err){
-    res.status(500).json(err)
-    return
-    }
-//PARSE STRING INTO JSON
-const noteData=JSON.parse(data)
-//PUSH OUR NEW NOTE INTO JSON
-noteData.push(newNote)
-//STRINGIFY db ARRAY AND SAVE FILE
-fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(noteData),function(err){
-    if(err){
-        res.status(500).json(err)
-        return
-    }
-    res.status(200).json(newNote)
-})
-})
-})
+const createNote = (body, noteData) => {
+  const newNote = body;
+  body.id = instance();
+  noteData.push(newNote);
+  fs.writeFileSync(path.join(__dirname, "db.json"), JSON.stringify(noteData));
+  console.log(body.id);
+  return newNote;
+};
+
 ///////////////////////////////////////////////////////////////
 // app.get('/api/db/:id',(req,res)=>{
 //     const animalType=req.params.id
@@ -100,14 +69,14 @@ fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(noteData),function(
 // }
 // //TO DO READ ANIMALS.JSON CONTENTS
 // fs.readFile(path.join(__dirname,"db.json"),'utf-8', function(err,data){
-    
+
 //     if(err){
 //         res.status(500).json(err)
 //         return
 //     }
 //     const noteData=JSON.parse(data)
 //     const results=noteData.filter(animal=> animal.type===animalType)
-    
+
 //     if(results.length===0){
 //         res.status(404)
 //     }
@@ -117,36 +86,29 @@ fs.writeFile(path.join(__dirname, "db.json"), JSON.stringify(noteData),function(
 // })
 /////////////////////////////////////////////////////////
 
-app.delete('/api/db/:id',(req,res)=>{
-    console.log('delete route hit')
-    const id =req.params.id
-    
-    if(!id){
-        res.status(400).json({error:"we need an id"})
-        return
+app.delete("/api/notes/:id", (req, res) => {
+  deleteNote(req.params.id, noteData);
+  res.json(true);
+});
+
+const deleteNote = (id, noteData) => {
+  for (let i = 0; i < noteData.length; i++) {
+    let note = noteData[i];
+    if (note.id == id) {
+      noteData.splice(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, "db.json"),
+        JSON.stringify(noteData)
+      );
+      break;
     }
-    //the process to actually delete:read the json file,modify the contents,stringify contents and resave the contents
-    fs.readFile(path.join(__dirname,"db.json"),'utf-8',function(error,data){
-        const noteData=JSON.parse(data)
-    console.log(noteData)
-    //return an array of all objects EXXCEPT the one you chose which will be deleted with .filter
-    const updatedNoteData=noteData.filter(note=> id !=note.id)
-    console.log(updatedNoteData)
-    fs.writeFile(path.join(__dirname,"db.json"),JSON.stringify(updatedNoteData),function(error){
-        if(error){
-    
-        return res.status(500).json(error)
-    }
-    res.json(true)
-    })
-    })
-    console.log('delete route hit')      //NOW CODE IN FRONT END INDEX>JS so front end code sends the ID to the delete route
-})
+  }
+};
 
 //////////////////////////////////////////////////////////////
 
-// // --the : creates a url parameter if you need a dynamic varible 
-// app.get('/api/animals/:animalType',(req,res)=>{     
+// // --the : creates a url parameter if you need a dynamic varible
+// app.get('/api/animals/:animalType',(req,res)=>{
 //     //    console.log(req.params)
 //     const results=noteData.filter(animal=>animal.type===req.params.animalType)
 //      if(results.length===0){
@@ -155,7 +117,6 @@ app.delete('/api/db/:id',(req,res)=>{
 //     res.json(results)
 //     })
 //////////////////////////////////////////////////////////////
-app.listen(PORT,()=>{
-
-    console.log(`listening on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`listening on http://localhost:${PORT}`);
+});
